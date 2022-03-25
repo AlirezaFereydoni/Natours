493cfe1ff4879c31d56aeef3ff2f3e1f5678408a
Alireza <Aliirzw@gmail.com>
2022-03-26 00:07:52 +0430

    Add DELETE and PUT api

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/tours", (req, res) => {
+// Get all tours
+const getAllTour = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
@@ -15,9 +16,25 @@ app.get("/api/tours", (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post("/api/tours", (req, res) => {
+// Get specific tour
+const getSpecificTour = (req, res) => {
+  const { id } = req.params;
+  const specificTour = tours.find(tour => tour.id === Number(id));
+
+  if (!specificTour) {
+    res.status(404).json({ status: "fail", message: "not found" });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: specificTour,
+  });
+};
+
+// Create a new tour
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id++;
   const newTour = { id: newId, ...req.body };
 
@@ -33,7 +50,51 @@ app.post("/api/tours", (req, res) => {
       data: newTour,
     });
   });
-});
+};
+
+// Update tour with PUT
+const updateTour = (req, res) => {
+  const { id } = req.params;
+  const updatedTours = tours.map(tour => (tour.id === Number(id) ? req.body : tour));
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    err => {
+      if (err) {
+        res.status(500).json({ status: "Fail", message: "something goes wrong..." });
+      }
+
+      res.status(200).json({
+        status: "Success",
+        data: {},
+      });
+    }
+  );
+};
+
+// Delete a tour
+const deleteTour = (req, res) => {
+  const { id } = req.params;
+
+  const updatedTours = tours.filter(tour => tour.id !== Number(id));
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    err => {
+      if (err) {
+        res.status(500).json({ status: "Fail", message: "something goes wrong" });
+      }
+
+      res.status(204).json({ status: "Success", data: null });
+    }
+  );
+};
+
+app.route("/api/tours").get(getAllTour).post(createTour);
+
+app.route("/api/tours/:id").get(getSpecificTour).put(updateTour).delete(deleteTour);
 
 app.listen(8000, "127.0.0.1", () => {
   console.log("Server running ...");
