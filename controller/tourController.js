19b7db6660dabe9b96e4a3704ba3323e4517b823
Alireza <Aliirzw@gmail.com>
2022-05-05 14:37:05 +0430

    Add Controllers to our app

---
+++ controller/tourController.js
@@ -0,0 +1,84 @@
+// Get all tours
+exports.getAllTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+// Get specific tour
+exports.getSpecificTour = (req, res) => {
+  const { id } = req.params;
+  const specificTour = tours.find(tour => tour.id === Number(id));
+
+  if (!specificTour) {
+    res.status(404).json({ status: 'fail', message: 'not found' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: specificTour,
+  });
+};
+
+// Create a new tour
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id++;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) {
+      res.status(400).json({ status: 'Fail', message: 'Bad Request', data: {} });
+    }
+
+    res.status(200).json({
+      status: 'Success',
+      data: newTour,
+    });
+  });
+};
+
+// Update tour with PUT
+exports.updateTour = (req, res) => {
+  const { id } = req.params;
+  const updatedTours = tours.map(tour => (tour.id === Number(id) ? req.body : tour));
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    err => {
+      if (err) {
+        res.status(500).json({ status: 'Fail', message: 'something goes wrong...' });
+      }
+
+      res.status(200).json({
+        status: 'Success',
+        data: {},
+      });
+    }
+  );
+};
+
+// Delete a tour
+exports.deleteTour = (req, res) => {
+  const { id } = req.params;
+
+  const updatedTours = tours.filter(tour => tour.id !== Number(id));
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    err => {
+      if (err) {
+        res.status(500).json({ status: 'Fail', message: 'something goes wrong' });
+      }
+
+      res.status(204).json({ status: 'Success', data: null });
+    }
+  );
+};
---
+++ controller/userController.js
@@ -0,0 +1,39 @@
+// Get All Users
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Something goes wrong',
+  });
+};
+
+// Get Specific User
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Something goes wrong',
+  });
+};
+
+// Create User
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Something goes wrong',
+  });
+};
+
+// Update User
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Something goes wrong',
+  });
+};
+
+// Delete User
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'Something goes wrong',
+  });
+};
