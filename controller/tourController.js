6cb5174a5e41a33325d6043c1653acedbb424adc
Alireza Fereydoni <Aliirzw@gmail.com>
2023-10-13 14:25:16 +0330

    feat: implement getAllTour and getSpecificTour

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,10 +1,31 @@
 const Tour = require('../models/tour.model');
 
 // Get all tours
-exports.getAllTour = (req, res) => {};
+exports.getAllTour = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err });
+  }
+};
 
 // Get specific tour
-exports.getSpecificTour = (req, res) => {};
+exports.getSpecificTour = async (req, res) => {
+  try {
+    const specificTour = await Tour.findById(req.params.id);
+
+    res.status(200).json({ status: 'success', data: specificTour });
+  } catch (err) {
+    res.status(404).json({ status: 'fail', message: err });
+  }
+};
 
 // Create a new tour
 exports.createTour = async (req, res) => {
