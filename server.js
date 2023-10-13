6399e2a0ecc0cb25d09cbcad5a294cadc0f85264
AlirezaFereydoni <Aliirzw@gmail.com>
2023-10-13 11:45:50 +0330

    feat: add mongoose and connect to DB

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
+const mongoose = require('mongoose');
 const app = require('./app');
 
-// Server
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB Connection Succeed!'));
+
+// Server
 const port = process.env.PORT || 3000;
 
 app.listen(port, '127.0.0.1', () => {
