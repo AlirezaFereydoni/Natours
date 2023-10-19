69e8ca4b7d45068a1a7fec3bbf7294cbf3cd5b29
Alireza Fereydoni <Aliirzw@gmail.com>
2023-10-19 11:13:49 +0330

    feat: add some field to tour schema

--- models/tour.model.js
+++ models/tour.model.js
@@ -5,23 +5,49 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Name is required'],
     unique: true,
+    trim: true,
   },
   price: {
     type: Number,
     required: [true, 'Price is required'],
   },
-  rating: {
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'Duration is required'],
   },
   difficulty: {
     type: String,
     required: false,
+    trim: true,
   },
-  available: {
-    type: Boolean,
-    default: true,
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Group size is required'],
   },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Summary is required'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: { type: String, required: [true, 'Image cover is required'] },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
