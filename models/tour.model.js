9a9cb2d8671b24cd229dfadac33c1f5182be2ef0
Alireza Fereydoni <Aliirzw@gmail.com>
2023-10-26 15:14:58 +0330

    feat: add weekDuration with virtual

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,53 +1,63 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Name is required'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      unique: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+    },
+    ratingAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Duration is required'],
+    },
+    difficulty: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Group size is required'],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Summary is required'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: { type: String, required: [true, 'Image cover is required'] },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  price: {
-    type: Number,
-    required: [true, 'Price is required'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  ratingAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'Duration is required'],
-  },
-  difficulty: {
-    type: String,
-    required: false,
-    trim: true,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Group size is required'],
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'Summary is required'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: { type: String, required: [true, 'Image cover is required'] },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+
+tourSchema.virtual('weekDuration').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
