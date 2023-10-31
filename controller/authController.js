a3178da29248525e05b59d1199c0176219da72b3
Alireza Fereydoni <Aliirzw@gmail.com>
2023-10-31 22:20:46 +0330

    feat: create token after signup

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,4 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const createResponse = require('../utils/createResponse');
@@ -9,7 +10,12 @@ const signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  createResponse(res, 201, newUser);
+
+  const token = JWT.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE_IN,
+  });
+
+  createResponse(res, 201, { newUser, token });
 });
 
 module.exports = {
