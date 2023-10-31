d6a785e8fa709282b39ca8cb0ba407552fe68209
Alireza Fereydoni <Aliirzw@gmail.com>
2023-10-31 21:47:17 +0330

    fix: prevent to set user role from outside

--- controller/authController.js
+++ controller/authController.js
@@ -3,7 +3,12 @@ const catchAsync = require('../utils/catchAsync');
 const createResponse = require('../utils/createResponse');
 
 const signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
   createResponse(res, 201, newUser);
 });
 
