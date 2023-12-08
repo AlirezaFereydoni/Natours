eee6c22741ddf14a0a1fbbbe657ceccb855f9fef
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-08 23:48:43 +0330

    feat: merge params into get reviews on tour
    Co-authored-by: Mahsa Jamshidian <maahjam@gmail.com>

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -7,13 +7,13 @@ const createReview = catchAsync(async (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
 
-  console.log(req.user);
-
   const newReview = await Review.create(req.body);
   createResponse(res, 201, newReview);
 });
 const getAllReview = catchAsync(async (req, res, next) => {
-  const filtered = filters(req.query, Review);
+  let filter = { ...req.query };
+  if (req.params.tourId) filter = { ...req.query, tour: req.params.tourId };
+  const filtered = filters(filter, Review);
   const sorted = sort(req.query, filtered);
   const reviews = await sorted;
   createResponse(res, 200, reviews);
