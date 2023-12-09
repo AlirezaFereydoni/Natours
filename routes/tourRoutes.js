e79e84c7c7ce63356e2cf8c6cf553701ea2db5ce
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-09 23:00:03 +0330

    fix: authorization of api in users and tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,23 +12,38 @@ router.use('/:tourId/reviews', ReviewRouter);
 // Routes
 
 router.route('/stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protected,
+    authController.restrictTo('tourLead', 'tourGuide', 'admin'),
+    tourController.getMonthlyPlan,
+  );
 
 router
   .route('/get-5-cheap')
-  .get(tourController.getFiveCheapMiddleware, tourController.getAllTour);
+  .get(
+    authController.protected,
+    authController.restrictTo('tourLead', 'tourGuide', 'admin'),
+    tourController.getFiveCheapMiddleware,
+    tourController.getAllTour,
+  );
 
 router
   .route('/')
-  .get(authController.protected, tourController.getAllTour)
+  .get(tourController.getAllTour)
   .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getSpecificTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protected,
+    authController.restrictTo('tourLead', 'tourGuide', 'admin'),
+    tourController.updateTour,
+  )
   .delete(
     authController.protected,
-    authController.restrictTo('admin', 'tourLead'),
+    authController.restrictTo('admin', 'tourLead', 'tourGuide'),
     tourController.deleteTour,
   );
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,9 @@ const {
   updateMe,
   deActiveMe,
   getUser,
+  setIdToParams,
   updateUser,
   deleteUser,
-  setIdToParams,
 } = require('../controller/userController');
 const authController = require('../controller/authController');
 
@@ -17,20 +17,17 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:resetToken', authController.resetPassword);
-router.patch(
-  '/changePassword',
-  authController.protected,
-  authController.changePassword,
-);
+
+router.use(authController.protected);
 
 // Users
-router.patch('/updateMe', authController.protected, updateMe);
-router.patch('/deActiveMe', authController.protected, deActiveMe);
-router.route('/').get(getAllUsers);
-router
-  .route('/me')
-  .get(setIdToParams, getUser)
-  .patch(setIdToParams, updateUser)
-  .delete(setIdToParams, deleteUser);
+router.patch('/changePassword', authController.changePassword);
+router.patch('/updateMe', updateMe);
+router.patch('/deActiveMe', deActiveMe);
+router.get('/me', setIdToParams, getUser);
+
+router.use(authController.restrictTo('admin'));
+
+router.route('/').get(getAllUsers).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
