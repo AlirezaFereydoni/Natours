88ec7c347e28850c3591e7648e267c38418be9df
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-09 22:02:58 +0330

    feat: add getOne and getAll factory function
    Co-authored-by: Mahsa Jamshidian <maahjam@gmail.com>

--- controller/factoryHandler.js
+++ controller/factoryHandler.js
@@ -1,11 +1,31 @@
 const catchAsync = require('../utils/catchAsync');
 const createResponse = require('../utils/createResponse');
 const errorHandler = require('../utils/errorHandler');
+const { filters, sort } = require('../utils/apiFeatures');
 
 const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const newDoc = await Model.create(req.body);
     createResponse(res, 201, newDoc);
+  });
+
+const getOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    const specificDoc = await Model.findById(req.params.id);
+
+    if (populateOptions) specificDoc.populate(populateOptions);
+
+    if (!specificDoc) errorHandler(404, "Document isn't find with this ID");
+    createResponse(res, 200, specificDoc);
+  });
+const getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const features = filters(req.query, Model);
+    const sorted = sort(req.query, features);
+
+    const docs = await sorted;
+
+    createResponse(res, 200, docs);
   });
 
 const updateOne = (Model) =>
@@ -25,4 +45,4 @@ const deleteOne = (Model) =>
     createResponse(res, 200, 'This document deleted successfully');
   });
 
-module.exports = { createOne, updateOne, deleteOne };
+module.exports = { createOne, getOne, getAll, updateOne, deleteOne };
