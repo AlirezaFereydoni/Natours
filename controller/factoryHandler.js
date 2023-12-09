0a69e154ba099fa0054de9bf3a8a9695dee3ea43
Alireza Fereydoni <Aliirzw@gmail.com>
2023-12-09 21:20:35 +0330

    feat: add updateUser and deleteUser
    Co-authored-by: Mahsa Jamshidian <maahjam@gmail.com>

--- controller/factoryHandler.js
+++ controller/factoryHandler.js
@@ -2,13 +2,6 @@ const catchAsync = require('../utils/catchAsync');
 const createResponse = require('../utils/createResponse');
 const errorHandler = require('../utils/errorHandler');
 
-const deleteOne = (Model) =>
-  catchAsync(async (req, res, next) => {
-    const doc = await Model.findByIdAndDelete(req.params.id);
-    if (!doc) errorHandler(404, "Document isn't find with this ID");
-    createResponse(res, 200, 'This document deleted successfully');
-  });
-
 const createOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const newDoc = await Model.create(req.body);
@@ -23,6 +16,13 @@ const updateOne = (Model) =>
     });
     if (!updatedDoc) errorHandler(404, "Document isn't find with this ID");
     createResponse(res, 200, updatedDoc);
+  });
+
+const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) errorHandler(404, "Document isn't find with this ID");
+    createResponse(res, 200, 'This document deleted successfully');
   });
 
 module.exports = { createOne, updateOne, deleteOne };
